9d0ae8fb86142e914972f98d3bb75390019a5f0e
Albymens <albertamensah.na@gmail.com>
2022-03-11 11:09:22 -0800

    customer mgt system setup

---
+++ src/main/java/dreamOvalTest/customerManagementSystem/CustomerManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package dreamOvalTest.customerManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerManagementSystemApplication.class, args);
+		System.out.println("Server is running");
+	}
+
+}
---
+++ src/test/java/dreamOvalTest/customerManagementSystem/CustomerManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dreamOvalTest.customerManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
